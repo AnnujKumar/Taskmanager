51f4655c80701220417716761dcf3c4be3e6fe53
AnnujKumar <annujinsan@gmail.com>
2025-04-16 17:29:24 +0530

    main

--- app.js
+++ app.js
@@ -17,52 +17,10 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(helmet());
-
-// Security: Content Security Policy (CSP)
-const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
-    "https://cdn.maptiler.com/",
-];
-const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net",
-    "https://cdn.maptiler.com/",
-];
-const connectSrcUrls = [
-    "https://api.maptiler.com/",
-];
-const fontSrcUrls = [];
-app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/dlqmb0jce/", // Replace with your Cloudinary account
-                "https://images.unsplash.com/",
-                "https://api.maptiler.com/",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        },
-    })
-);
 
 // Security: CORS (Cross-Origin Resource Sharing)
 app.use(cors());
+
 
 // Security: Rate Limiting
 const limiter = rateLimit({
@@ -72,11 +30,7 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
-// Security: Prevent NoSQL Injection
-app.use(mongoSanitize());
 
-// Security: Prevent XSS Attacks
-app.use(xss());
 
 // Routes
 app.use('/user', userRoutes);
